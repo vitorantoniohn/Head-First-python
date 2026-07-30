a59684f664273e846724672cdbd00f907c6ca2e5
Vitor Antônio Henriques Negreiros <vitorantonio2005@gmail.com>
2026-07-30 01:33:22 +0000

    changes

--- exercicio1.py
+++ exercicio1.py
@@ -1,12 +1,14 @@
 import urllib.request
+import time
 
+price = 99.99
 
- page = urllib.request.urlopen("https://www.youtube.com/")
-text = page.read().decode("utf8")
-where = text.find('>$')
-start_of_price = where + 2
-end_of_price = start_of_price + 4
-
-price = text[start_of_price : end_of_price]
-
-print(price)
+while price > 4.47:  
+    time.sleep(1000)  # Wait for 60 seconds before checking the price again
+    page = urllib.request.urlopen("https://www.youtube.com/")
+    text = page.read().decode("utf8")
+    where = text.find('>$')
+    start_of_price = where + 2
+    end_of_price = start_of_price + 4
+    price = float(text[start_of_price : end_of_price])
+print("Buy!") 
